f43d8c2e40eeab3a3d41adff59002f9aced83e49
Jack@ <148102997+JackAt4@users.noreply.github.com>
2023-12-05 11:14:06 -0600

    Added Some Elements
    They unfortunetly don't function though

--- Scenes/People/player.gd
+++ Scenes/People/player.gd
@@ -1,12 +1,15 @@
 extends CharacterBody2D
 
-const SPEED = 325.0
+const speed = 325.0
 
 func handleInput():
 	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	velocity = moveDirection * SPEED
+	velocity = moveDirection * speed
+
+func _process(delta):
+	if Input.is_action_pressed("ui_accept"):
+		pass
 
 func _physics_process(delta):
-	
 	handleInput()
 	move_and_slide()
