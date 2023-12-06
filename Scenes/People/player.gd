59b51dd4cbe6252e00174bbacdf3d2421fce11bc
Jack@ <148102997+JackAt4@users.noreply.github.com>
2023-12-05 22:00:35 -0600

    Added code for picking up (W.I.P)

--- Scenes/People/player.gd
+++ Scenes/People/player.gd
@@ -1,15 +1,25 @@
 extends CharacterBody2D
 
 const speed = 325.0
+var keyPressed = false
+var holding = false
 
 func handleInput():
 	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = moveDirection * speed
 
-func _process(delta):
+func _process(_delta):
 	if Input.is_action_pressed("ui_accept"):
-		pass
+		if not keyPressed and not holding:
+			if str($Area2D.get_overlapping_bodies().back()).begins_with("Box"):
+				holding = true
+			keyPressed = true
+		elif not keyPressed and holding:
+			holding = false
+		print(holding)
+	else:
+		keyPressed = false
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	handleInput()
 	move_and_slide()
