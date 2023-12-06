19c4500f041bf057cf89f1c2e8f9fb77e19e5d6c
Jack@ <148102997+JackAt4@users.noreply.github.com>
2023-12-06 11:14:50 -0600

    Picking up Boxes (Still W.I.P)

--- Scenes/People/player.gd
+++ Scenes/People/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 const speed = 325.0
 var keyPressed = false
 var holding = false
+var box
 
 func handleInput():
 	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
@@ -11,15 +12,24 @@ func handleInput():
 func _process(_delta):
 	if Input.is_action_pressed("ui_accept"):
 		if not keyPressed and not holding:
-			if str($Area2D.get_overlapping_bodies().back()).begins_with("Box"):
+			box = $Area2D.get_overlapping_bodies().back()
+			print(box)
+			if str(box).begins_with("Box"):
 				holding = true
-			keyPressed = true
+				box.get_parent().remove_child(box)
+				add_child(box)
+				box.set_global_position(self.get_position() + Vector2(125, 125))
+			print(str(holding) + ": Top")
 		elif not keyPressed and holding:
 			holding = false
-		print(holding)
+			remove_child(box)
+			get_parent().add_child(box)
+			print(str(holding) + ": Bottom")
+		keyPressed = true
 	else:
 		keyPressed = false
 
 func _physics_process(_delta):
 	handleInput()
 	move_and_slide()
+ 
