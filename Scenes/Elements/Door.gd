62e0f74333392c4dda4fefdb496a22bf2cb29167
Jack@ <148102997+JackAt4@users.noreply.github.com>
2023-12-07 11:12:55 -0600

    Basics Done

--- Scenes/Elements/Door.gd
+++ Scenes/Elements/Door.gd
@@ -3,10 +3,10 @@ extends StaticBody2D
 func _on_button_body_entered(body):
 	if body.get_collision_layer() == 4:
 		hide()
-		
+		$CollisionShape2D.set_deferred("disabled", true)
 
 
 func _on_button_body_exited(body):
 	if body.get_collision_layer() == 4:
 		show()
-		
+		$CollisionShape2D.set_deferred("disabled", false)
